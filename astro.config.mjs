6eb3c40bf6822066f684bd35c71138bd3c7b5c67
André Ahlert <andre@aex.partners>
2026-04-09 17:02:31 -0300

    Atualizar site URL para org solgreen-org
    Signed-off-by: André Ahlert <andre@aex.partners>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://andreahlert.github.io',
+  site: 'https://solgreen-org.github.io',
   base: '/solgreen-site',
   integrations: [react()],
 
